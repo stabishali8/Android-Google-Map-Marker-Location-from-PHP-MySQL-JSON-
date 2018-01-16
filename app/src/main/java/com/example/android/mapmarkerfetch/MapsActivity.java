1b30b9d1bca7be133eecf8ff456d67c8982f8543
stabishali8 <stabishali8@gmail.com>
2018-01-16 18:50:50 +0500

    initial version

---
+++ app/src/main/java/com/example/android/mapmarkerfetch/MapsActivity.java
@@ -0,0 +1,166 @@
+package com.example.android.mapmarkerfetch;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    //Google Map
+    private GoogleMap mMap;
+    private final static int MY_PERMISSION_FINE_LOCATION = 101;
+
+    // Latitude & Longitude
+    private Double Latitude = 0.00;
+    private Double Longitude = 0.00;
+    ArrayList<HashMap<String, String>> location = new ArrayList<HashMap<String, String>>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        //*** Permission StrictMode
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+         }
+//        ArrayList<HashMap<String, String>> location = null;
+        String url = "http://192.168.5.126/MapPractice/show_data.php";
+        try {
+
+            JSONArray data = new JSONArray(getHttpGet(url));
+//            location = new ArrayList<HashMap<String, String>>();
+            HashMap<String, String> map;
+
+            for(int i = 0; i < data.length(); i++){
+                JSONObject c = data.getJSONObject(i);
+
+                map = new HashMap<String, String>();
+                map.put("LocationID", c.getString("LocationID"));
+                map.put("Latitude", c.getString("Latitude"));
+                map.put("Longitude", c.getString("Longitude"));
+                map.put("LocationName", c.getString("LocationName"));
+                location.add(map);
+            }
+        } catch (JSONException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    private String getHttpGet(String url) {
+        StringBuilder str = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) { // Download OK
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    str.append(line);
+                }
+            } else {
+                Log.e("Log", "Failed to download result..");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str.toString();
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        LatLng karachi = new LatLng(25.0700, 67.2848);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(karachi));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            mMap.setMyLocationEnabled(true);
+        } else {
+//
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_FINE_LOCATION);
+            }
+        }
+        // *** Marker (Loop)
+        for (int i = 0; i < location.size(); i++) {
+            Latitude = Double.parseDouble(location.get(i).get("Latitude").toString());
+            Longitude = Double.parseDouble(location.get(i).get("Longitude").toString());
+            String name = location.get(i).get("LocationName").toString();
+            MarkerOptions marker = new MarkerOptions().position(new LatLng(Latitude, Longitude)).title(name);
+            mMap.addMarker(marker);
+        }
+
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSION_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        mMap.setMyLocationEnabled(true);
+                        Toast.makeText(this.getApplicationContext(),"map gps",Toast.LENGTH_LONG).show();
+                    }
+                }else {
+                    Toast.makeText(getApplicationContext(),"This is require",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
+}
